5d3009219407e601bffc84ebc89ea5bcbb839f69
Shea Gonyo <shea@illcreative.com>
2015-08-19 07:59:24 -0400

    updated warden

--- config/initializers/warden.rb
+++ config/initializers/warden.rb
@@ -2,9 +2,12 @@
 Warden::Manager.after_set_user except: :fetch do |user, auth, opts|
   if auth.cookies.signed[:guest_token].present?
     if user.is_a?(Spree::User)
-      Spree::Order.where(email: user.email, guest_token: auth.cookies.signed[:guest_token], user_id: nil).each do |order|
-        order.associate_user!(user)
+      old_order = Spree::Order.where(guest_token: auth.cookies.signed[:guest_token], user_id: nil).first
+      if user.last_incomplete_spree_order.present?
+        user.last_incomplete_spree_order.empty!
       end
+
+      old_order.associate_user!(user)
     end
   end
 end
